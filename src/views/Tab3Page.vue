b85cb0a66b03eb7d11ce306bc39aac2696562606
marson parulian <marson.parulian@gmail.com>
2022-05-16 21:29:26 +1000

    init ionic components

--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -2,28 +2,59 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 3</ion-title>
+        <ion-title>Ionic Components</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 3</ion-title>
+          <ion-title size="large">Content Ionic Components</ion-title>
         </ion-toolbar>
       </ion-header>
-      
-      <ExploreContainer name="Tab 3 page" />
+
+      <div>
+        <h2>Button</h2>
+        <ion-button>Ionic Button</ion-button>
+      </div>
+      <div>
+        <h2>Toggle</h2>
+        <ion-toggle></ion-toggle>
+      </div>
+      <div>
+        <h2>Date Picker</h2>
+        <ion-datetime></ion-datetime>
+      </div>
+      <div>
+        <h2>Segment</h2>
+        <ion-segment>
+          <ion-segment-button value="short">
+            <ion-label>Short</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="normal">
+            <ion-label>Normal</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="long">
+            <ion-label>Long</ion-label>
+          </ion-segment-button>
+        </ion-segment>
+      </div>
+
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  IonLabel, IonSegmentButton, IonSegment, IonToggle, IonButton, IonDatetime,
+  IonPage, IonHeader, IonToolbar, IonTitle, IonContent
+} from '@ionic/vue';
 
 export default defineComponent({
   name: 'Tab3Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: {
+    IonLabel, IonSegmentButton, IonSegment, IonToggle, IonButton, IonDatetime,
+    IonHeader, IonToolbar, IonTitle, IonContent, IonPage
+  }
 });
 </script>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -7,15 +7,15 @@
           <ion-icon :icon="triangle" />
           <ion-label>Tab 1</ion-label>
         </ion-tab-button>
-          
+
         <ion-tab-button tab="tab2" href="/tabs/tab2">
           <ion-icon :icon="ellipse" />
           <ion-label>Tab 2</ion-label>
         </ion-tab-button>
-        
+
         <ion-tab-button tab="tab3" href="/tabs/tab3">
           <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-label>Ion Components</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -32,8 +32,8 @@ export default defineComponent({
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
   setup() {
     return {
-      ellipse, 
-      square, 
+      ellipse,
+      square,
       triangle,
     }
   }
