299d5d16e7d20ed59e733cf0fd35c7e3062ac838
marson parulian <marson.parulian@gmail.com>
2022-05-18 23:53:17 +1000

    addd more props for 'MovieView'

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -1,12 +1,23 @@
 <template>
     <div>
         <h3>{{ Name }}</h3>
+        <img class="poster" :src="SkinMobileUrl" :alt="`movie poster of ${Name}`" />
         <div>Genres : <span v-for="g in MovieGenres" :key="g.Id">{{ g.Name }}</span> </div>
         <div>Director : {{ Director || '-' }}</div>
+        <div>Main casts : {{ MainCast }}</div>
+        <div>Released : {{ ReleasedAt }}</div>
+        <div>Running time : {{ RunningTime }}</div>
         <h4>Synopsis :</h4>
         <div>{{ Synopsis }}</div>
     </div>
 </template>
+<style>
+img.poster {
+    float: left;
+    margin: 0 20px 20px 0;
+    width: 250px;
+}
+</style>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
@@ -20,6 +31,10 @@ export default defineComponent({
         Director: String,
         Synopsis: String,
         MovieGenres: movieGenres,
+        ReleasedAt: String,
+        RunningTime: Number,
+        MainCast: String,
+        SkinMobileUrl: String,
     },
 });
 </script>
