2bfa6af0eda58f2f92b87ca8707d311843457dcb
marson parulian <marson.parulian@gmail.com>
2022-05-25 12:29:40 +1000

    fix : remove unused title

--- src/views/MoviesPage.vue
+++ src/views/MoviesPage.vue
@@ -2,7 +2,7 @@
     <ion-page>
         <ion-header>
             <ion-toolbar>
-                <ion-title>Movies : {{ genre }}</ion-title>
+                <ion-title>Movies</ion-title>
             </ion-toolbar>
         </ion-header>
         <ion-content :fullscreen="true">
