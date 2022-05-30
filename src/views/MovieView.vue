c98fd20924eef9f3af6e7adb583c1ed51af8aedb
marson parulian <marson.parulian@gmail.com>
2022-05-30 23:57:02 +1000

    style : apply max height & faded content to movie cards

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -10,15 +10,17 @@
             <ion-card-subtitle>Director : {{ Director || '-' }}</ion-card-subtitle>
             <ion-card-subtitle>Main casts : {{ MainCast || '-' }}</ion-card-subtitle>
         </ion-card-header>
-        <ion-card-content>
-            <h4>Synopsis</h4>
-            {{ Synopsis }}
-        </ion-card-content>
-        <ion-card-content>
-            <div>Released at : {{ ReleasedAt ? new Date(Date.parse(ReleasedAt)).toLocaleDateString('en-AU') : '-' }}
-            </div>
-            <div>Running time : {{ `${RunningTime} minutes` || '-' }}</div>
-        </ion-card-content>
+        <div class="faded">
+            <ion-card-content>
+                <div>Released at : {{ ReleasedAt ? new Date(Date.parse(ReleasedAt)).toLocaleDateString('en-AU') : '-' }}
+                </div>
+                <div>Running time : {{ `${RunningTime} minutes` || '-' }}</div>
+            </ion-card-content>
+            <ion-card-content>
+                <h4>Synopsis</h4>
+                <div class="synopsis">{{ Synopsis }}</div>
+            </ion-card-content>
+        </div>
     </ion-card>
 </template>
 
@@ -46,3 +48,58 @@ export default defineComponent({
     },
 });
 </script>
+
+<style>
+.faded {
+    /* scroll bar width, for use in mask calculations */
+    --scrollbar-width: 4px;
+
+    /* mask fade distance, for use in mask calculations */
+    --mask-height: 32px;
+
+    /* If content exceeds height of container, overflow! */
+    overflow-y: auto;
+
+    /* Our height limit */
+    height: 320px;
+
+    /* Need to make sure container has bottom space,
+  otherwise content at the bottom is always faded out */
+    padding-bottom: var(--mask-height);
+    padding-top: var(--mask-height);
+
+    /* Keep some space between content and scrollbar */
+    padding-right: 12px;
+
+    /* The CSS mask */
+
+    /* The content mask is a linear gradient from top to bottom */
+    --mask-image-content: linear-gradient(to bottom,
+            transparent,
+            black var(--mask-height),
+            black calc(100% - var(--mask-height)),
+            transparent);
+
+    /* Here we scale the content gradient to the width of the container 
+  minus the scrollbar width. The height is the full container height */
+    --mask-size-content: calc(100% - var(--scrollbar-width)) 100%;
+
+    /* The scrollbar mask is a black pixel */
+    --mask-image-scrollbar: linear-gradient(black, black);
+
+    /* The width of our black pixel is the width of the scrollbar.
+  The height is the full container height */
+    --mask-size-scrollbar: var(--scrollbar-width) 100%;
+
+    /* Apply the mask image and mask size variables */
+    mask-image: var(--mask-image-content), var(--mask-image-scrollbar);
+    mask-size: var(--mask-size-content), var(--mask-size-scrollbar);
+
+    /* Position the content gradient in the top left, and the 
+  scroll gradient in the top right */
+    mask-position: 0 0, 100% 0;
+
+    /* We don't repeat our mask images */
+    mask-repeat: no-repeat, no-repeat;
+}
+</style>
