53971cfd30e1a64ca633a8c904b830f1927685ad
marson parulian <marson.parulian@gmail.com>
2022-06-04 21:28:46 +1000

    style <MovieView />

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -1,39 +1,28 @@
 <template>
-    <ion-card>
+    <ion-card class="movie-view">
         <ion-progress-bar v-if="posterFetchStatus === 2" type="indeterminate"></ion-progress-bar>
-        <img v-if="poster" :src="poster" />
+        <img v-if="poster" :src="poster" @Click="$emit('movieSelected', Id)" />
         <ion-card-header>
-            <ion-chip v-for="g in MovieGenres" :key="g.Id" :routerLink="`/tabs/movies/${g.Name.toLowerCase()}`">
-                <ion-label>{{ g.Name }}</ion-label>
-            </ion-chip>
-            <ion-card-title  @Click="$emit('movieSelected', Id)">{{ Name }}</ion-card-title>
-            <ion-card-subtitle>Director : {{ Director || '-' }}</ion-card-subtitle>
-            <ion-card-subtitle>Main casts : {{ MainCast || '-' }}</ion-card-subtitle>
+            <ion-card-title @Click="$emit('movieSelected', Id)">{{ Name }}</ion-card-title>
+            <ion-card-subtitle v-if="Director">{{ Director }}</ion-card-subtitle>
+            <ion-card-subtitle v-if="MainCast">{{ MainCast }}</ion-card-subtitle>
         </ion-card-header>
-        <div class="faded">
-            <ion-card-content>
-                <div>Released at : {{ ReleasedAt ? new Date(Date.parse(ReleasedAt)).toLocaleDateString('en-AU') : '-' }}
-                </div>
-                <div>Running time : {{ `${RunningTime} minutes` || '-' }}</div>
-            </ion-card-content>
-            <ion-card-content>
-                <h4>Synopsis</h4>
-                <div class="synopsis">{{ Synopsis }}</div>
-            </ion-card-content>
-        </div>
+        <ion-card-content>
+            <ion-label>{{ RunningTime ? `${RunningTime} minutes` : '' }}</ion-label>
+        </ion-card-content>
     </ion-card>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonProgressBar, IonChip, IonLabel, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent } from '@ionic/vue';
+import { IonProgressBar, IonLabel, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent } from '@ionic/vue';
 import { FetchStatus } from '../types/common';
 
 const movieGenres: any = Array;
 
 export default defineComponent({
     name: 'MovieView',
-    components: { IonProgressBar, IonChip, IonLabel, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent },
+    components: { IonProgressBar, IonLabel, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent },
     emits: ['movieSelected'],
     props: {
         Id: Number,
@@ -52,6 +41,17 @@ export default defineComponent({
 </script>
 
 <style>
+.movie-view {
+    background: black;
+    text-align: center;
+}
+
+.movie-view ion-card-title {
+    color: white;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-weight: 300;
+}
+
 .faded {
     /* scroll bar width, for use in mask calculations */
     --scrollbar-width: 4px;
